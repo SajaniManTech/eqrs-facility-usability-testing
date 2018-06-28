44273e365e3b567e52c4592f82a91d0b20f1179e
Sajani Patel <spatel@nwed.esrd.net>
2018-06-28 10:21:31 -0400

    added facility-search component

---
+++ src/app/facility-search/facility-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-facility-search',
+  templateUrl: './facility-search.component.html',
+  styleUrls: ['./facility-search.component.css']
+})
+export class FacilitySearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
