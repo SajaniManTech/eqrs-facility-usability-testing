ce111341d4d57f66bd4902e5b7500bce6620f2b7
Sajani Patel <spatel@nwed.esrd.net>
2018-07-11 15:55:11 -0400

    removed eqrs dependency

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
 import { FacilitySearchResultsComponent } from './facility-search-results/facility-search-results.component';
 import {routing} from './app-routing.module';
-import {DynamicDatatableModule} from '@eqrs/datatable';
 
 @NgModule({
   declarations: [
@@ -26,7 +25,6 @@ import {DynamicDatatableModule} from '@eqrs/datatable';
     MdInputModule,
     MdTooltipModule,
     HttpClientModule,
-    DynamicDatatableModule,
     routing
   ],
   providers: [HttpClient],
